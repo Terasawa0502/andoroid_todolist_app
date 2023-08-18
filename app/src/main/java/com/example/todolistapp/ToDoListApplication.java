625da80933bf1a6a67cabcaa0247f5e81f19285c
寺澤徳隆 <mosqey1889@gmail.com>
2023-08-18 22:58:12 +0900

    ToDoListApplicationクラスの作成

---
+++ app/src/main/java/com/example/todolistapp/ToDoListApplication.java
@@ -0,0 +1,10 @@
+package com.example.todolistapp;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class ToDoListApplication extends Application {
+    // 未実装
+}
