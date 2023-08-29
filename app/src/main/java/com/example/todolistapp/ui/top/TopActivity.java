dc125f3e766d0bdb0b72b1c20f3c69f6c2c89b77
寺澤徳隆 <mosqey1889@gmail.com>
2023-08-29 22:20:09 +0900

    BottomSheetの追加

--- app/src/main/java/com/example/todolistapp/ui/top/TopActivity.java
+++ app/src/main/java/com/example/todolistapp/ui/top/TopActivity.java
@@ -9,12 +9,15 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.todolistapp.R;
 import com.example.todolistapp.data.TodoDatabase;
 import com.example.todolistapp.data.dao.TodoSheetDao;
 import com.example.todolistapp.data.entities.TodoSheet;
 import com.example.todolistapp.ui.top.adapter.TodoSheetPagerAdapter;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
@@ -29,14 +32,24 @@ public class TopActivity extends AppCompatActivity {
     private ViewPager2 viewPager;
     private TopViewModel topViewModel;
     private FloatingActionButton btn;
+    private View bottomSheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomSheet = findViewById(R.id.bottom_sheet);
+        BottomSheetBehavior behavior = BottomSheetBehavior.from(bottomSheet);
+
+        Button bottomSheetCloseBtn = findViewById(R.id.bottom_sheet_close_btn);
+        bottomSheetCloseBtn.setOnClickListener(v -> {
+            behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        });
+
         btn = findViewById(R.id.btn);
-        btn.setOnClickListener(view ->{
-            topViewModel.insertTestSample(this);
+        btn.setOnClickListener(view -> {
+            behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+//            topViewModel.insertTestSample(this);
         });
         // ViewModelの生成
         topViewModel = new ViewModelProvider(this).get(TopViewModel.class);
