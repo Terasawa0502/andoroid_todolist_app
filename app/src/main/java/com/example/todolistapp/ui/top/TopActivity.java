8f66d676f1d611eec57485ab0c03d13d47fdea18
寺澤徳隆 <mosqey1889@gmail.com>
2023-09-12 05:39:39 +0900

    NavigationDrawerの設定

--- app/src/main/java/com/example/todolistapp/ui/top/TopActivity.java
+++ app/src/main/java/com/example/todolistapp/ui/top/TopActivity.java
@@ -1,7 +1,10 @@
 package com.example.todolistapp.ui.top;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.room.Room;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
@@ -11,12 +14,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.todolistapp.R;
 import com.example.todolistapp.data.TodoDatabase;
 import com.example.todolistapp.data.dao.TodoSheetDao;
 import com.example.todolistapp.data.entities.TodoSheet;
 import com.example.todolistapp.ui.top.adapter.TodoSheetPagerAdapter;
+import com.example.todolistapp.util.KeyboardUtil;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
@@ -31,30 +36,16 @@ public class TopActivity extends AppCompatActivity {
 
     private ViewPager2 viewPager;
     private TopViewModel topViewModel;
-    private FloatingActionButton btn;
-    private View bottomSheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomSheet = findViewById(R.id.bottom_sheet);
-        BottomSheetBehavior behavior = BottomSheetBehavior.from(bottomSheet);
+        // ナビゲーションの設定
+        initNavigation();
+        // ボトムシートの設定
+        bottomSheetAndFabSettings();
 
-        // FloatingActionButtonが押されたと時の挙動
-        btn = findViewById(R.id.btn);
-        btn.setOnClickListener(view -> {
-            behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-            btn.setVisibility(View.INVISIBLE);
-//            topViewModel.insertTestSample(this);
-        });
-
-        // BottomSheetPageのCloseButtonが押された時の挙動
-        Button bottomSheetCloseBtn = findViewById(R.id.bottom_sheet_close_btn);
-        bottomSheetCloseBtn.setOnClickListener(v -> {
-            behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-            btn.setVisibility(View.VISIBLE);
-        });
         // ViewModelの生成
         topViewModel = new ViewModelProvider(this).get(TopViewModel.class);
 
@@ -78,4 +69,24 @@ public class TopActivity extends AppCompatActivity {
             }
         }).attach();
     }
+
+    /**
+     * ナビデーションドロワーの設定
+     */
+    private void initNavigation() {
+        // Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // DrawerToggle
+        DrawerLayout drawer = findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,drawer,toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close
+        );
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+    
 }
