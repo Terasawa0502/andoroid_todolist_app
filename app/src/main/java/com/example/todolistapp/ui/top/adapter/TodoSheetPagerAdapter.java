149e3cd20dac7c064c85d68a9d4c672d67438d6c
寺澤徳隆 <mosqey1889@gmail.com>
2023-10-26 22:39:41 +0900

    Adapterの修正

--- app/src/main/java/com/example/todolistapp/ui/top/adapter/TodoSheetPagerAdapter.java
+++ app/src/main/java/com/example/todolistapp/ui/top/adapter/TodoSheetPagerAdapter.java
@@ -5,24 +5,40 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import com.example.todolistapp.ui.top.ToDoSheetFragement;
+import com.example.todolistapp.data.entities.TodoSheet;
+import com.example.todolistapp.ui.top.ToDoSheetFragment;
+
+import java.util.List;
 
 public class TodoSheetPagerAdapter extends FragmentStateAdapter {
 
+    private List<TodoSheet> todoSheetList;
     // コンストラクター
-    public TodoSheetPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+    public TodoSheetPagerAdapter(@NonNull FragmentActivity fragmentActivity, List<TodoSheet> todoSheetList) {
         super(fragmentActivity);
+        this.todoSheetList = todoSheetList;
     }
 
+    public List<TodoSheet> getTodoSheetList() {
+        return todoSheetList;
+    }
+
+    public void updateTodoSheetList(List<TodoSheet> todoSheetList) {
+        this.todoSheetList = todoSheetList;
+        // データ更新用メソッド
+        notifyDataSetChanged();
+    }
     // pageを作るときに呼ばれる処理
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        return ToDoSheetFragement.newInstance();
+        TodoSheet data = todoSheetList.get(position);
+        // TODO: 後でやる
+        return ToDoSheetFragment.newInstance();
     }
 
     @Override
     public int getItemCount() {
-        return 3;
+        return todoSheetList.size();
     }
 }
