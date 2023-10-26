0e067f1367ed4ac263f67e5b8fbbfffa9231a713
寺澤徳隆 <mosqey1889@gmail.com>
2023-10-26 22:42:36 +0900

    Repositoryの設定

--- app/src/main/java/com/example/todolistapp/data/repositories/ToDoListRepository.java
+++ app/src/main/java/com/example/todolistapp/data/repositories/ToDoListRepository.java
@@ -3,13 +3,16 @@ package com.example.todolistapp.data.repositories;
 import android.content.Context;
 import androidx.room.Room;
 import com.example.todolistapp.data.TodoDatabase;
+import com.example.todolistapp.data.dao.TodoDao;
 import com.example.todolistapp.data.dao.TodoSheetDao;
+import com.example.todolistapp.data.entities.Todo;
 import com.example.todolistapp.data.entities.TodoSheet;
 import java.util.List;
 
 public class ToDoListRepository {
 
     private TodoDatabase db;
+    private TodoDao todoDao;
     private TodoSheetDao todoSheetDao;
 
     public ToDoListRepository(Context context) {
@@ -17,13 +20,21 @@ public class ToDoListRepository {
                 context, TodoDatabase.class,
                 "todolist-db"
         ).build();
+        todoDao = db.todoDao();
         todoSheetDao = db.todoSheetDao();
+    }
+
+    public void insertTodoItem(Todo item) {
+        todoDao.insert(item);
     }
 
     public void insertTodoSheet(TodoSheet data) {
         todoSheetDao.insert(data);
     }
 
+    public List<Todo> getTodoListById(int id) {
+        return todoDao.getTodoListById(id);
+    }
     public List<TodoSheet> getAllTodoSheet() {
         return todoSheetDao.getAll();
     }
