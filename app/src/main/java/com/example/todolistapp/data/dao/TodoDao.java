11ee8a50e353f88719b8d0ec2ec96d74f6827e73
寺澤徳隆 <mosqey1889@gmail.com>
2023-10-26 22:41:07 +0900

    Todoデータベースの生成

--- app/src/main/java/com/example/todolistapp/data/dao/TodoDao.java
+++ app/src/main/java/com/example/todolistapp/data/dao/TodoDao.java
@@ -4,14 +4,20 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
 
 import com.example.todolistapp.data.entities.Todo;
+
+import java.util.List;
 
 @Dao
 public interface TodoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Todo todo);
+
+    @Query("SELECT * FROM todo WHERE list_id = :id")
+    List<Todo> getTodoListById(int id);
 
     @Delete
     void delete(Todo todo);
