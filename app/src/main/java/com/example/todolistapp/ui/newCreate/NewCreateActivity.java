40f17df3e128ed066b52606db06ab77f0fb9257f
寺澤徳隆 <mosqey1889@gmail.com>
2023-11-04 11:15:08 +0900

    NewCreateActivityのキャンセル処理の実装

--- app/src/main/java/com/example/todolistapp/ui/newCreate/NewCreateActivity.java
+++ app/src/main/java/com/example/todolistapp/ui/newCreate/NewCreateActivity.java
@@ -3,6 +3,7 @@ package com.example.todolistapp.ui.newCreate;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,12 +17,20 @@ public class NewCreateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newcreate);
 
-        findViewById(R.id.exit_btn).setOnClickListener(v ->{
+        // シート追加ボタンを押した時の挙動
+        findViewById(R.id.add_btn).setOnClickListener(v ->{
+
             Intent intent = new Intent();
             intent.putExtra(KEY_TITLE, "NewSheet");
             setResult(Activity.RESULT_OK, intent);
             finish();
         });
+
+        // キャンセルボタンを押した時の挙動
+        findViewById(R.id.cancel_btn).setOnClickListener(v -> {
+            finish();
+        });
+
     }
 
     // editテキストを取得して、クリックリスナーに定義
